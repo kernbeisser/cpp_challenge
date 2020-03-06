17c9bdfaadae630482c85319166fc65e42f4527f
kernbeisser <kernbeisser@tom-bonn.de>
2020-03-06 09:47:47 +0100

    greatest_prime: changed output added sexy_prime

--- src/sexy_prime.cpp
+++ src/sexy_prime.cpp
@@ -13,20 +13,28 @@ bool is_prime(unsigned long number){
     return true;
 }
 
+bool is_sexy(unsigned long p1, unsigned long p2){
+    if(p2 - p1 == 6)
+        return true;
+    return false;
+}
 
 int main(){
 
     unsigned long n;
     std::cout << "Give us a number! ";
     std::cin >> n;
-    int p {};
+    int p1 {};
+    int p2 {};
 
     for(auto i = 2; i <= n; ++i) 
     {
-        if(is_prime(i))
-            p = i;
+        if(is_prime(i) && is_prime(i+6)){
+            p1 = i;
+            p2 = i+6;
+            std::cout << "Sexy prime pair: (" << p1 << ", " << p2 << ")\n";
+        }
     }
-    std::cout << "Greatest prime number less than " << n << " is " << p << "\n";
 
     return 0;
 }
