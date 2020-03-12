df1172d1b0f70e0e5be6bdd21d4ccf9ca9759bc6
kernbeisser <kernbeisser@tom-bonn.de>
2020-03-12 13:19:39 +0100

    a more structured version now; decoding simplyfied

--- src/gray-code.cpp
+++ src/gray-code.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<iomanip>
 #include<bitset>
+#include<sstream>
 
 /*
  * Quelle: wikipedia.de
@@ -18,20 +19,36 @@
  *
  * */
 
+unsigned int enc(unsigned int n)
+{
+    return n ^ (n >> 1);
+}
+
+unsigned int decode(unsigned int g)
+{
+    for(unsigned int i = 32; i > 1; i = (i >> 1))
+    {
+        if(g & i) g = g ^ (i >> 1);
+    }
+    return g;
+}
+
+std::string print_line(unsigned int i, std::bitset<5> bin, std::bitset<5> enc, unsigned int dec)
+{
+    std::stringstream out {};
+    out << std::setw(2) << i << " " << bin << " " << enc << " " << std::setw(2) << dec;
+    return out.str();
+}
+
 int main(){
 
-    // TODO use a function for converting
-    for(unsigned long i = 0; i < 32; ++i){
+    for(unsigned int i = 0; i < 32; ++i){
         std::bitset<5> binary {i};
-        auto x3 = (i ^ (i >> 1));
-        std::bitset<5> gray {x3};
+        auto x3 = enc(i);
+        std::bitset<5> x3_enc {x3};
+        auto x3_dec = decode(x3);
 
-        // TODO write that in a string stream
-        std::cout << std::setw(2) << i << " " 
-                  << binary << " " 
-                  << gray << " " 
-                  << std::setw(2) 
-                  << x3 <<"\n";
+        std::cout << print_line(i, binary, x3_enc, x3_dec) << "\n";
     }
 
     return 0;
